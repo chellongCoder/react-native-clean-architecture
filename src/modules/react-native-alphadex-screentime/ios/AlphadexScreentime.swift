ba61702fa78b2e9754d578a8df286e332c78a08c
Nguyễn Nhật Long <longnn22@viettel.com.vn>
2024-05-28 21:22:30 +0700

    chore: Add unBlockApps function to react-native-alphadex-screentime module

--- src/modules/react-native-alphadex-screentime/ios/AlphadexScreentime.swift
+++ src/modules/react-native-alphadex-screentime/ios/AlphadexScreentime.swift
@@ -22,7 +22,7 @@ class ScreenTimeSelectAppsModel: NSObject, ObservableObject {
 
   let store = ManagedSettingsStore(named: .mySettingStore)
 
-  @Published var activitySelection: FamilyActivitySelection
+  @Published var activitySelection = FamilyActivitySelection(includeEntireCategory: true)
   @Published var selectionToDiscourage = FamilyActivitySelection() {
       willSet {
         let applications = newValue.applicationTokens
@@ -67,9 +67,91 @@ class ScreenTimeSelectAppsModel: NSObject, ObservableObject {
     }
   }
 
-  override init() {
+  @objc(blockApps:withRejecter:)
+  func blockApps(resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    let store = ManagedSettingsStore()
+    let userDefaults = UserDefaults.init(suiteName: "group.com.hisoft.tbd.app")!
+    let data = userDefaults.data(forKey: AlphadexScreentime.userDefaultsKey)
+    if data != nil {
+      let decodedData = try? decoder.decode(
+        FamilyActivitySelection.self,
+        from: data!
+      )
+      if let unwrapped = decodedData {
+        store.shield.applicationCategories = ShieldSettings.ActivityCategoryPolicy.specific(unwrapped.categoryTokens)
+        store.shield.applications = unwrapped.applicationTokens
+        userDefaults.set(true, forKey:"blocked")
+        //            sendEvent("onChangeBlocked", [
+        //              "isBlocked": true
+        //            ]);
+        //            if #available(iOS 16.2, *) {
+        //                let activity = try Activity.request(
+        //                    attributes: screentimewidgetAttributes(name: "Text1"),
+        //                    content: .init(state: screentimewidgetAttributes.ContentState(emoji: "Blocked"), staleDate: nil),
+        //                    pushType: .token
+        //                )
+        //            }
+        //            if #available(iOS 14.0, *) {
+        //              WidgetCenter.shared.reloadAllTimelines()
+        //            }
+      }
+    }
+  }
 
-    activitySelection = FamilyActivitySelection()
+  func startAppRestrictions() -> Void {
+    let store = ManagedSettingsStore()
+    let userDefaults = UserDefaults.init(suiteName: "group.com.hisoft.tbd.app")!
+    let data = userDefaults.data(forKey: AlphadexScreentime.userDefaultsKey)
+    if data != nil {
+      let decodedData = try? decoder.decode(
+        FamilyActivitySelection.self,
+        from: data!
+      )
+      if let unwrapped = decodedData {
+        store.shield.applicationCategories = ShieldSettings.ActivityCategoryPolicy.specific(unwrapped.categoryTokens)
+        store.shield.applications = unwrapped.applicationTokens
+        userDefaults.set(true, forKey:"blocked")
+        //            sendEvent("onChangeBlocked", [
+        //              "isBlocked": true
+        //            ]);
+        //            if #available(iOS 16.2, *) {
+        //                let activity = try Activity.request(
+        //                    attributes: screentimewidgetAttributes(name: "Text1"),
+        //                    content: .init(state: screentimewidgetAttributes.ContentState(emoji: "Blocked"), staleDate: nil),
+        //                    pushType: .token
+        //                )
+        //            }
+        //            if #available(iOS 14.0, *) {
+        //              WidgetCenter.shared.reloadAllTimelines()
+        //            }
+      }
+    }
+  }
+
+  @objc(unBlockApps:withRejecter:)
+  func unBlockApps(resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    let store = ManagedSettingsStore()
+    let userDefaults = UserDefaults.init(suiteName: "group.com.hisoft.tbd.app")!
+    store.shield.applicationCategories = ShieldSettings.ActivityCategoryPolicy.none
+    store.shield.applications = Set()
+    userDefaults.set(false, forKey:"blocked")
+    resolve(true)
+//    sendEvent("onChangeBlocked", [
+//      "isBlocked": false
+//    ]);
+//      if #available(iOS 16.2, *) {
+//        if let activity = Activity<screentimewidgetAttributes>.activities.first {
+//          await activity.end(ActivityContent(state: screentimewidgetAttributes.ContentState(emoji: "closing"), staleDate: nil), dismissalPolicy: .immediate)
+//        }
+//      }
+//    if #available(iOS 14.0, *) {
+//      WidgetCenter.shared.reloadAllTimelines()
+//    }
+  }
+
+
+
+  override init() {
   }
 }
 
