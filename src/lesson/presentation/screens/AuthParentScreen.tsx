f752277e9826f2d92ccebd7448ab6b38ec42d8ab
duy <duydq2k1@gmail.com>
2024-06-18 00:50:59 +0700

    add auth parent screen

--- src/lesson/presentation/screens/AuthParentScreen.tsx
+++ src/lesson/presentation/screens/AuthParentScreen.tsx
@@ -2,9 +2,27 @@ import {ScrollView, StyleSheet, View} from 'react-native';
 import React, {useState} from 'react';
 import {CommonInputPassword} from 'src/authentication/presentation/components/CommonInput';
 import PrimaryButton from '../components/PrimaryButton';
+import {replaceScreen} from 'src/core/presentation/navigation/actions/RootNavigationActions';
+import {STACK_NAVIGATOR} from 'src/core/presentation/navigation/ConstantNavigator';
+import {withProviders} from 'src/core/presentation/utils/withProviders';
+import {LessonStoreProvider} from '../stores/LessonStore/LessonStoreProvider';
+import {LessonStore} from '../stores/LessonStore/LessonStore';
+import {lessonModuleContainer} from 'src/lesson/LessonModule';
 
 const AuthParentScreen = () => {
   const [password, setPassword] = useState('');
+
+  const lessonStore = lessonModuleContainer.getProvided(LessonStore);
+
+  const onSubmit = () => {
+    if (password.length === 4) {
+      lessonStore.setPasswordParent(password);
+      replaceScreen(STACK_NAVIGATOR.PARENT.PARENT_SCREEN);
+    } else {
+      console.log(password);
+    }
+  };
+
   return (
     <View style={[styles.container]}>
       <ScrollView>
@@ -16,13 +34,17 @@ const AuthParentScreen = () => {
           }}
         />
 
-        <PrimaryButton text="Enter" style={styles.btnEnter} />
+        <PrimaryButton
+          text="Enter"
+          style={styles.btnEnter}
+          onPress={onSubmit}
+        />
       </ScrollView>
     </View>
   );
 };
 
-export default AuthParentScreen;
+export default withProviders(LessonStoreProvider)(AuthParentScreen);
 
 const styles = StyleSheet.create({
   container: {
