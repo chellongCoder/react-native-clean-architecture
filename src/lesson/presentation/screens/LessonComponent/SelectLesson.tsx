a2fc2174400f0d8802cf1a007567ba29a3325b89
Phan Văn Phú <nostarwhere.pc@gmail.com>
2025-01-02 15:39:21 +0000

    init selectLesson

---
+++ src/lesson/presentation/screens/LessonComponent/SelectLesson.tsx
@@ -0,0 +1,451 @@
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {
+  forwardRef,
+  useCallback,
+  useContext,
+  useEffect,
+  useImperativeHandle,
+  useMemo,
+  useState,
+} from 'react';
+import LessonComponent from './LessonComponent';
+import PrimaryButton from '../../components/PrimaryButton';
+import {FontFamily} from 'src/core/presentation/hooks/useFonts';
+import useGlobalStyle from 'src/core/presentation/hooks/useGlobalStyle';
+import {Task} from 'src/home/application/types/GetListQuestionResponse';
+import {COLORS} from 'src/core/presentation/constants/colors';
+import {getCorrectAnswer, WIDTH_SCREEN} from 'src/core/presentation/utils';
+import {scale, verticalScale} from 'react-native-size-matters';
+import Animated, {
+  Easing,
+  ReduceMotion,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+import {TextToSpeechContext} from 'src/core/presentation/hooks/textToSpeech/TextToSpeechContext';
+import {useLessonStore} from '../../stores/LessonStore/useGetPostsStore';
+import {useSettingLesson} from '../../hooks/useSettingLesson';
+import {useIsFocused} from '@react-navigation/native';
+import useAuthenticationStore from 'src/authentication/presentation/stores/useAuthenticationStore';
+import {observer} from 'mobx-react';
+import {LessonRef} from '../../types';
+import useHomeStore from 'src/home/presentation/stores/useHomeStore';
+
+type Props = {
+  moduleIndex: number;
+  totalModule: number;
+  nextModule: (e: string) => void;
+  lessonName: string;
+  moduleName: string;
+  firstMiniTestTask?: Task;
+  backgroundImage?: string;
+  characterImageSuccess?: string;
+  characterImageFail?: string;
+  isMulti?: boolean;
+  question: string;
+  answer?: string[];
+};
+
+const SelectLesson = observer(
+  forwardRef<LessonRef, Props>(
+    (
+      {
+        moduleIndex,
+        nextModule,
+        totalModule,
+        lessonName,
+        moduleName,
+        firstMiniTestTask,
+        backgroundImage,
+        characterImageSuccess,
+        characterImageFail,
+        isMulti,
+        question,
+        answer,
+      },
+      ref,
+    ) => {
+      const globalStyle = useGlobalStyle();
+
+      const {ttsSpeak} = useContext(TextToSpeechContext);
+      const focus = useIsFocused();
+
+      const [answerSelected, setAnswerSelected] = useState<string | string[]>(
+        '',
+      );
+      const {trainingCount, getSetting} = useLessonStore();
+
+      const {selectedChild} = useAuthenticationStore();
+
+      const {
+        isAnswerCorrect,
+        isShowCorrectContainer,
+        word,
+        env,
+        learningTimer,
+        submit,
+        toggleShowHint,
+        resetLearning,
+      } = useSettingLesson({
+        countDownTime: trainingCount <= 2 ? 0 : 5,
+        isCorrectAnswer:
+          answerSelected ===
+          getCorrectAnswer(
+            firstMiniTestTask?.question?.[moduleIndex]?.correctAnswer,
+          ),
+        onSubmit: () => {
+          setAnswerSelected(isMulti ? [] : '');
+          nextModule(answerSelected);
+        },
+        fullAnswer: firstMiniTestTask?.question?.[moduleIndex].fullAnswer,
+      });
+
+      const {lessonSetting} = useHomeStore();
+
+      const settings = useMemo(
+        () => getSetting(lessonSetting),
+        [getSetting, lessonSetting],
+      );
+      const characterImage = useMemo(() => {
+        return isAnswerCorrect === true || isAnswerCorrect === undefined
+          ? characterImageSuccess
+          : characterImageFail;
+      }, [characterImageFail, characterImageSuccess, isAnswerCorrect]);
+
+      const onSpeechText = useCallback(() => {
+        ttsSpeak?.(
+          firstMiniTestTask?.question?.[moduleIndex].fullAnswer
+            .toString()
+            .toLowerCase() ?? '',
+        );
+      }, [firstMiniTestTask?.question, moduleIndex, ttsSpeak]);
+
+      const onSelectAnswer = useCallback(
+        (e: string) => {
+          if (isMulti) {
+            setAnswerSelected(prev => {
+              if (Array.isArray(prev) && prev.includes(e)) {
+                return prev.filter(ans => ans !== e); // Deselect
+              }
+              return Array.isArray(prev) ? [...prev, e] : [e]; // Select
+            });
+          } else {
+            setAnswerSelected(e); // Single selection
+          }
+        },
+        [isMulti],
+      );
+
+      const opacity = useSharedValue(0);
+      const scaleS = useSharedValue(1);
+
+      /**
+       * * reset lại countdown khi lần làm thay đổi
+       */
+      useEffect(() => {
+        resetLearning();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, [trainingCount]);
+
+      useEffect(() => {
+        if (focus) {
+          // Check if the component is focused
+          const firstTimeout = setTimeout(() => {
+            onSpeechText();
+
+            const secondTimeout = setTimeout(() => {
+              onSpeechText();
+            }, 2500);
+
+            return () => clearTimeout(secondTimeout);
+          }, 1500);
+
+          return () => clearTimeout(firstTimeout);
+        }
+      }, [onSpeechText, focus]); // Added focus to the dependency array
+
+      useEffect(() => {
+        opacity.value = withTiming(0, {duration: 500}, () => {
+          opacity.value = withTiming(1, {duration: 500});
+        });
+        scaleS.value = withTiming(0, {duration: 500}, () => {
+          scaleS.value = withTiming(1, {
+            duration: 500,
+            easing: Easing.elastic(2),
+            reduceMotion: ReduceMotion.System,
+          });
+        });
+      }, [moduleIndex, opacity, scaleS]);
+
+      const animatedStyle = useAnimatedStyle(() => {
+        return {
+          opacity: opacity.value,
+          transform: [{scale: scaleS.value}],
+        };
+      });
+
+      useImperativeHandle(ref, () => ({
+        isAnswerCorrect,
+        onChoiceCorrectedAnswer: () => {
+          setAnswerSelected(
+            getCorrectAnswer(
+              firstMiniTestTask?.question?.[moduleIndex]?.correctAnswer,
+            ),
+          );
+        },
+      }));
+
+      return (
+        <LessonComponent
+          backgroundImage={backgroundImage}
+          characterImage={characterImage}
+          lessonName={lessonName}
+          module={moduleName}
+          part={firstMiniTestTask?.name}
+          backgroundColor="#66c270"
+          backgroundAnswerColor={
+            settings.backgroundAnswerColor ?? COLORS.GREEN_DDF598
+          }
+          prompt={settings.prompt?.toString()}
+          price="Free"
+          score={selectedChild?.adsPoints}
+          isAnswerCorrect={isAnswerCorrect}
+          isShowCorrectContainer={isShowCorrectContainer}
+          onPressFlower={toggleShowHint}
+          buildQuestion={
+            <View>
+              <Text style={[styles.fonts_SVN_Cherish, styles.textQuestion]}>
+                {word}
+              </Text>
+              <Animated.Image
+                resizeMode={'contain'}
+                width={WIDTH_SCREEN}
+                height={scale(200)}
+                style={[{}, animatedStyle]}
+                source={{
+                  uri:
+                    env.IMAGE_QUESTION_BASE_API_URL +
+                    firstMiniTestTask?.question?.[moduleIndex].image,
+                }}
+              />
+            </View>
+          }
+          buildAnswer={
+            <View style={styles.fill}>
+              <View style={styles.wrapHeaderContainer}>
+                <View
+                  style={{
+                    justifyContent: 'center',
+                    flex: 1,
+                  }}>
+                  <Text style={[globalStyle.txtLabel, styles.textColor]}>
+                    Choose the correct answer
+                  </Text>
+                </View>
+
+                <TouchableOpacity onPress={onSpeechText}>
+                  <Image
+                    source={require('../../../../../assets/images/icon_speech.png')}
+                    style={styles.iconImageContainer}
+                  />
+                </TouchableOpacity>
+              </View>
+              <View style={[styles.boxSelected]}>
+                <View style={styles.wrapCharContainer}>
+                  <Text
+                    style={[
+                      styles.fonts_SVN_Neu,
+                      styles.textQuestion,
+                      styles.textGreen,
+                      styles.mt8,
+                      {fontSize: 24},
+                    ]}>
+                    {question}
+                  </Text>
+                </View>
+                <View style={[styles.wapper, styles.fill, {width: '100%'}]}>
+                  {answer?.map((e, i) => {
+                    const bg =
+                      Array.isArray(answerSelected) &&
+                      answerSelected.includes(e)
+                        ? isShowCorrectContainer && !isAnswerCorrect
+                          ? '#F28759'
+                          : '#66C270'
+                        : '#F2B559';
+                    const length = answer?.length ?? 2;
+                    const size = Math.min(
+                      (WIDTH_SCREEN - 48) / Math.ceil(length / 2),
+                      verticalScale(72),
+                    );
+
+                    return (
+                      <TouchableOpacity
+                        key={i}
+                        onPress={() => onSelectAnswer(e)}
+                        style={[
+                          styles.boxVowel,
+                          {
+                            backgroundColor: bg,
+                            height: size,
+                            width: size,
+                            margin: 8, // Add spacing for clarity
+                          },
+                        ]}>
+                        <Text style={[styles.textVowel]}>{e}</Text>
+                      </TouchableOpacity>
+                    );
+                  })}
+                </View>
+                {learningTimer !== 0 && (
+                  <View
+                    style={[
+                      styles.boxSelected,
+                      {
+                        position: 'absolute',
+                        zIndex: 999,
+                        width: '100%',
+                        opacity: 0.7,
+                        height: '100%',
+                      },
+                    ]}
+                  />
+                )}
+              </View>
+
+              <PrimaryButton
+                text="Submit"
+                style={[styles.mt24]}
+                onPress={submit}
+              />
+            </View>
+          }
+          moduleIndex={moduleIndex}
+          totalModule={totalModule}
+        />
+      );
+    },
+  ),
+);
+
+export default SelectLesson;
+
+const styles = StyleSheet.create({
+  fill: {
+    flex: 1,
+  },
+  fonts_SVN_Cherish: {
+    fontFamily: FontFamily.SVNCherishMoment,
+  },
+  fonts_SVN_Neu: {
+    fontFamily: FontFamily.SVNNeuzeitRegular,
+  },
+  textColor: {
+    color: '#1C6349',
+  },
+  textLarge: {
+    fontSize: 140,
+    textAlign: 'center',
+    color: 'white',
+  },
+  textQuestion: {
+    fontSize: verticalScale(34),
+    textAlign: 'center',
+    color: COLORS.BLUE_258F78,
+  },
+  textGreen: {
+    color: '#258F78',
+  },
+  txtWhite: {
+    color: 'white',
+  },
+  rowAround: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  rowAlignCenter: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  rowBetween: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  pr16: {
+    paddingRight: 16,
+  },
+  ph24: {
+    paddingHorizontal: 24,
+  },
+  pb8: {
+    paddingBottom: verticalScale(8),
+  },
+  pb16: {
+    paddingBottom: verticalScale(16),
+  },
+  pb32: {
+    paddingBottom: verticalScale(32),
+  },
+  mt8: {
+    marginTop: verticalScale(8),
+  },
+  mt16: {
+    marginTop: verticalScale(16),
+  },
+  mt24: {
+    marginTop: verticalScale(24),
+  },
+  mt32: {
+    marginTop: verticalScale(32),
+  },
+  alignSelfCenter: {
+    alignSelf: 'center',
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  boxItemAnswer: {
+    height: 94,
+    backgroundColor: '#F2B559',
+    borderRadius: 30,
+  },
+  boxSelected: {
+    backgroundColor: COLORS.WHITE_FBF8CC,
+    height: verticalScale(220),
+    flex: 1,
+    borderRadius: scale(30),
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  boxVowel: {
+    width: 56,
+    height: 56,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 6,
+    marginVertical: 6,
+  },
+  textVowel: {
+    fontFamily: FontFamily.SVNCherishMoment,
+    color: '#FBF8CC',
+    fontSize: verticalScale(28),
+  },
+  wapper: {
+    marginTop: 8,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent: 'center',
+    flexWrap: 'wrap', // Add this to enable wrapping
+  },
+  wrapCharContainer: {
+    flexDirection: 'row',
+  },
+  wrapHeaderContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 8,
+  },
+  iconImageContainer: {height: 45, width: 40},
+});
