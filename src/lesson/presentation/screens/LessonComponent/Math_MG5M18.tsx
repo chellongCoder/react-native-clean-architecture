24b0ca4559e6eae12072904b243400c901dd150a
JeffPhan <72235802+phanvanphuhn@users.noreply.github.com>
2025-03-04 08:36:09 +0700

    fixbug math 5

--- src/lesson/presentation/screens/LessonComponent/Math_MG5M18.tsx
+++ src/lesson/presentation/screens/LessonComponent/Math_MG5M18.tsx
@@ -122,8 +122,11 @@ const Math_MG2M4 = observer(
       }, [answerSelected]);
 
       const onSpeechText = useCallback(() => {
-        ttsSpeak?.(settings.prompt?.toString().toLowerCase() ?? '');
-      }, [settings.prompt, ttsSpeak]);
+        ttsSpeak?.(
+          firstMiniTestTask?.question?.[moduleIndex]?.instruction
+            ?.description ?? '',
+        );
+      }, [firstMiniTestTask?.question, moduleIndex, ttsSpeak]);
 
       const opacity = useSharedValue(0);
       const scaleS = useSharedValue(1);
