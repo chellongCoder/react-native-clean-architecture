8a9f64b9550054db9975c54fa9a7cdc32864a1ca
duy <duydq2k1@gmail.com>
2025-03-12 14:43:53 +0700

    fix(english): fix voice g4

--- src/lesson/presentation/screens/LessonComponent/English_EG4M23.tsx
+++ src/lesson/presentation/screens/LessonComponent/English_EG4M23.tsx
@@ -111,11 +111,8 @@ const English_EG4M23 = observer(
       }, [characterImageFail, characterImageSuccess, isAnswerCorrect]);
 
       const onSpeechText = useCallback(() => {
-        ttsSpeak?.(
-          firstMiniTestTask?.question?.[moduleIndex].fullAnswer.toString() ??
-            '',
-        );
-      }, [firstMiniTestTask?.question, moduleIndex, ttsSpeak]);
+        ttsSpeak?.(settings.prompt?.toString() ?? '');
+      }, [settings.prompt, ttsSpeak]);
 
       const opacity = useSharedValue(0);
       const scaleS = useSharedValue(1);
