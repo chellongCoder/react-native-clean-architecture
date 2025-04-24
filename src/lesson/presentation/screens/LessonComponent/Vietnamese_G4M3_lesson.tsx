feddb18576e01fa270883027bd6dca23851cdfb2
duy <duydq2k1@gmail.com>
2025-04-24 16:24:13 +0700

    fix cannot drag module 3+

--- src/lesson/presentation/screens/LessonComponent/Vietnamese_G4M3_lesson.tsx
+++ src/lesson/presentation/screens/LessonComponent/Vietnamese_G4M3_lesson.tsx
@@ -117,6 +117,7 @@ const VnG4M3Lesson = observer(
         onSubmit: () => {
           clear();
           setAnswerSelected('');
+          setIsCorrectAnswer(undefined);
           nextModule(answerSelected);
           answerRef.current?.resetAnswerSelected?.();
         },
@@ -208,10 +209,8 @@ const VnG4M3Lesson = observer(
       /**
        * * submit khi đúng
        */
-      const isSubmitRef = useRef(false);
       useEffect(() => {
-        if (isCorrectAnswer !== undefined && !isSubmitRef.current) {
-          isSubmitRef.current = true;
+        if (isCorrectAnswer !== undefined) {
           submit();
         }
       }, [isCorrectAnswer, submit]);
@@ -297,7 +296,7 @@ const VnG4M3Lesson = observer(
 
       const buildItemAnswer = useCallback(
         (items: string[], item: string, index: number) => {
-          console.log('🛠 LOG: 🚀 --> ~ item:', item);
+          // console.log('🛠 LOG: 🚀 --> ~ item:', item);
           return (
             <DragItem
               index={100 + index}
