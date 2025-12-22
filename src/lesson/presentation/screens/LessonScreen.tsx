e1ac8e7b0b6e001e7fe30d60ad49d4f0716417d8
JeffPhan <sean.nguyen@cebupacificair.com>
2025-12-22 20:24:55 +0700

    add mandarin 3

--- src/lesson/presentation/screens/LessonScreen.tsx
+++ src/lesson/presentation/screens/LessonScreen.tsx
@@ -386,17 +386,17 @@ const LESSON_PATTERNS = [
     props: {},
   },
   {
-    pattern: /^(writing|MANDARIN_MDG[1-6]M(1|4|7|10|13))$/,
+    pattern: /^(writing|MANDARIN_MDG[1-6]M(1|4|7|10|13|16|19|22|25|28|31))$/,
     component: Mandarin_G4M_DrawCharacter,
     props,
   },
   {
-    pattern: /^MANDARIN_MDG[1-6]M(2|5|8|11|14)$/,
+    pattern: /^MANDARIN_MDG[1-6]M(2|5|8|11|14|17|20|23|26|29|32)$/,
     component: Mandarin_G4M_SelectAnswer,
     props,
   },
   {
-    pattern: /^MANDARIN_MDG[1-6]M(3|6|9|12|15)$/,
+    pattern: /^MANDARIN_MDG[1-6]M(3|6|9|12|15|18|21|24|27|30|33)$/,
     component: Mandarin_G4_Pronunciation,
     props,
   },
